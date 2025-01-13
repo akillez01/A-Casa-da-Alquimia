7e023bf14758b0d66150ab980ba09bec571133de
akillez01 <achilles.oliveira.souza@gmail.com>
2025-01-13 12:36:02 -0500

    Configuração para GitHub Pages e ajustes de dependências

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configuração para otimização de imagens
   images: {
-    domains: ["images.pexels.com"],
-    loader: "akamai",
-    path: "",
-    unoptimized: true,
+    domains: ["images.pexels.com"], // Domínios permitidos para carregamento de imagens externas
+    loader: "akamai", // Loader de imagem para exportação estática
+    path: "", // Caminho base para imagens
+    unoptimized: true, // Desativa otimização de imagens para export estático
   },
+  // Configuração para exportação estática
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/A-Casa-da-Alquimia" : "",
   assetPrefix: process.env.NODE_ENV === "production" ? "/A-Casa-da-Alquimia/" : "",
+  
+  // Variáveis de ambiente
   env: {
     MONGO_URI: process.env.URL_MONGO,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
@@ -18,6 +22,9 @@ const nextConfig = {
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
   },
+
+  // Configuração para resolver problemas relacionados à exportação estática
+  trailingSlash: true, // Adiciona uma barra ao final dos caminhos
 };
 
 module.exports = nextConfig;
