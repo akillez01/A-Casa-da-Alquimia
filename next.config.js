09947a245b3ae5042725b372c8b1362ddcf862f0
akillez01 <achilles.oliveira.souza@gmail.com>
2025-01-13 12:11:22 -0500

    Atualiza configuração para exportação estática

--- next.config.js
+++ next.config.js
@@ -6,9 +6,18 @@ const nextConfig = {
     path: "",
     unoptimized: true,
   },
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/A-Casa-da-Alquimia' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/A-Casa-da-Alquimia/' : '',
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/A-Casa-da-Alquimia" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/A-Casa-da-Alquimia/" : "",
+  env: {
+    MONGO_URI: process.env.URL_MONGO,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+  },
 };
 
 module.exports = nextConfig;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,28 +3,55 @@ import Link from "next/link";
 import styles from "./footer.module.css";
 
 const Footer = () => {
+  // Define o basePath para garantir que os caminhos das imagens funcionem em produção
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
   return (
     <div className={styles.container}>
       <div>©2025 A Casa da Alquimia. All rights reserved.</div>
       <div className={styles.social}>
+        {/* Facebook */}
         <Link href="https://www.facebook.com/casadaalquimia/?locale=pt_BR" passHref legacyBehavior>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/1.png" width={24} height={24} className={styles.icon} alt="A casa da Alquimia Facebook Account" />
+            <Image
+              src={`${basePath}/1.png`}
+              width={24}
+              height={24}
+              alt="A casa da Alquimia Facebook Account"
+            />
           </a>
         </Link>
+        {/* Instagram */}
         <Link href="https://www.instagram.com/casadaalquimia?igsh=bXp2anV1ejQwbGUy" passHref legacyBehavior>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/2.png" width={24} height={24} className={styles.icon} alt="A casa da Alquimia Instagram Account" />
+            <Image
+              src={`${basePath}/2.png`}
+              width={24}
+              height={24}
+              alt="A casa da Alquimia Instagram Account"
+            />
           </a>
         </Link>
+        {/* WhatsApp */}
         <Link href="https://wa.me/+5562996538902" passHref legacyBehavior>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/icons8-whatsapp-office-l/icons8-whatsapp-40.png" width={24} height={24} className={styles.icon} alt="A casa da Alquimia WhatsApp Account" />
+            <Image
+              src={`${basePath}/icons8-whatsapp-office-l/icons8-whatsapp-40.png`}
+              width={24}
+              height={24}
+              alt="A casa da Alquimia WhatsApp Account"
+            />
           </a>
         </Link>
+        {/* YouTube */}
         <Link href="https://www.youtube.com/@ACasadaAlquimia" passHref legacyBehavior>
           <a target="_blank" rel="noopener noreferrer">
-            <Image src="/4.png" width={24} height={24} className={styles.icon} alt="A casa da Alquimia Youtube Account" />
+            <Image
+              src={`${basePath}/4.png`}
+              width={24}
+              height={24}
+              alt="A casa da Alquimia YouTube Account"
+            />
           </a>
         </Link>
       </div>
